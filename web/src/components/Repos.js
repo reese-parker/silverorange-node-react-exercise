060247cc673600025e5a70342359530b837a1bca
Reese Parker <parker.rhys@outlook.com>
2022-05-08 12:17:50 -0400

    Add state variables for repos data and get request loading status. Render a result if successful or a retry button if unsuccessful

--- web/src/components/Repos.js
+++ web/src/components/Repos.js
@@ -1,17 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Repos() {
+  const [repos, setRepos] = useState([]);
+  const [loadingStatus, setLoadingStatus] = useState('idle');
   const fetchReposFromServer = () => {
+    setLoadingStatus('loading');
     axios
       .get('http://localhost:4000/repos')
-      .then((response) => response.data)
-      .catch((err) => err);
+      .then((response) => {
+        setLoadingStatus('succeeded');
+        setRepos(response.data);
+      })
+      .catch((err) => {
+        setLoadingStatus('failed');
+        return err;
+      });
   };
 
   useEffect(() => {
     fetchReposFromServer();
   }, []);
 
-  return <div>Repos</div>;
+  return (
+    <div>
+      <p>{loadingStatus}</p>
+
+      {/* Renders retry button if get request fails */}
+      {loadingStatus === 'failed' && (
+        <button onClick={fetchReposFromServer}>Try again</button>
+      )}
+
+      {/* Renders first repo to confirm data */}
+      {repos.length > 0 && <p>{repos[0].name}</p>}
+    </div>
+  );
 }
