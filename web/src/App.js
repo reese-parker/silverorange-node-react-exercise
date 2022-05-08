253a7d29eee314dc170bf259892999b1aed585a6
Reese Parker <parker.rhys@outlook.com>
2022-05-08 12:01:28 -0400

    Import Repos component

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Repos from './components/Repos';
 
 export function App() {
   return (
     <div className="App">
       <h1>Repos</h1>
+      <Repos />
     </div>
   );
 }
